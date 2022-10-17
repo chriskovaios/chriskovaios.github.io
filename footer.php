57ce2ed613ec1d693a367dbbc24096f6f9f8833c
Chris Kovaios <chriskovaios@gmail.com>
2022-10-17 16:47:14 +0300

    no php commit

--- footer.php
+++
@@ -1,7 +0,0 @@
-<?php echo'<div class="tcenter"><font color="#222536">© 2021 ARES Conference</font></div>';
-if (isset($_SESSION['UserID'])){
-	
-	echo "<a href='logout.php' class='myli' style='margin-right:0px;margin-bottom:0px;position:fixed;'>Logout</a>";
-	}
-
-?>
